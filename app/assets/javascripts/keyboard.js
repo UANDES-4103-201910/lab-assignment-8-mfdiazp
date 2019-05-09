78c15e9fa9150253aff48aa1a93a6eaa45089c5d
Martín Díaz <mfdiaz1@miuandes.cl>
2019-05-09 19:48:45 -0400

    Keyboard now logs key presses to console.

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,7 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
 $(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+    $( ".key" ).click(function( event ) {
+        console.log( $( this ).html() );
+ 
+    });
 });
